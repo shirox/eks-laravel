014ccbb0d56ec3bb6178956c631282ee123d96db
Shirota <j_shirota@innovation.co.jp>
2018-12-05 03:46:27 +0900

    site welcome view title change

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Shirox Web
                 </div>
 
                 <div class="links">
